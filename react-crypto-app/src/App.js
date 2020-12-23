a1fdf7bfa092583f501be5c2cb1d1cce4e522f99
Yago Ananias <yago_souza92@yahoo.com.br>
2020-12-23 18:32:16 -0300

    Criando busca

--- react-crypto-app/src/App.js
+++ react-crypto-app/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+
+
+
 
 function App() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
+      setCoins(res.data)
+    }).catch(error => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-        <h1>API</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Procure uma criptomoeda</h1>
+        <form>
+          <input type="text" placeholder="Procurar" className="coin-input"/>
+        </form>
+      </div>
+      
     </div>
   );
 }
